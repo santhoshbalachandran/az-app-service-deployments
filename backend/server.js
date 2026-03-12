dd9dae78cb9b03f8ac9b8bf051bb21412acd012e
Santhosh Balachandran <sbalachandr5@csc.com>
2026-03-12 09:19:09 +0530

    fix path for azure

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,10 @@ app.get("/api", (req, res) => {
   res.send("API working");
 });
 
-app.use(express.static("../frontend"));
+// app.use(express.static("../frontend"));
+
+// static frontend
+app.use(express.static(path.join(__dirname, "..", "frontend")));
 
 const port = process.env.PORT || 3000
 
