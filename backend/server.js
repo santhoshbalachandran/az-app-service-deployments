f537cde8ca32e4fdec1a6dacac2a404d387f53e2
Santhosh Balachandran <sbalachandr5@csc.com>
2026-03-12 09:03:43 +0530

    fix routing order

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("../frontend"));
-
 app.get("/api", (req, res) => {
   res.send("API working");
 });
+
+app.use(express.static("../frontend"));
 
 const port = process.env.PORT || 3000
 
